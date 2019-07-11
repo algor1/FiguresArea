08456e2d9fcff68476fa94c367f168013592b5fa
algor1 <alexeev.igor.v@gmail.com>
2019-07-11 16:28:37 +0300

    add test  IsRightTriangleTest_2_2_sqrt8_true_returned

--- FiguresAreaLibTests/FiguresAreaLibTests.cs
+++ FiguresAreaLibTests/FiguresAreaLibTests.cs
@@ -13,18 +13,22 @@ namespace FigureasArea.Tests
     [TestClass()]
     public class FiguresAreaLibTests
     {
-
+        private FiguresAreaLib figuresAreaLib;
         
-
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            figuresAreaLib = new FiguresAreaLib();
+        }
+        
         [TestMethod()]
         public void CircleAreaTest2_4returned()
         {
             //arrange
             double r = 2;
             double expected = 8*Math.PI;
-            FiguresAreaLib f = new FiguresAreaLib();
             //act
-            double actiual = f.CircleArea(r);
+            double actiual = figuresAreaLib.CircleArea(r);
             //assert
             Assert.AreEqual(expected, actiual);
         }
@@ -38,13 +42,26 @@ namespace FigureasArea.Tests
             double c = Math.Sqrt(8);
             double expected = 2;
             double delta= 0.0000001;
-            FiguresAreaLib f = new FiguresAreaLib();
+            
             //act
-            double actiual = f.TriangleArea(a,b,c);
+            double actiual = figuresAreaLib.TriangleArea(a, b, c);
 
             //assert
             Assert.AreEqual(expected, actiual,delta);
             
+        }
+        [TestMethod()]
+        public void IsRightTriangleTest_2_2_sqrt8_true_returned()
+        {
+            //arrange
+            double a = 2;
+            double b = 2;
+            double c = Math.Sqrt(8);
+            //act
+            double actiual = figuresAreaLib.IsRightTriangle(a, b, c);
+
+            //assert
+            Assert.IsTrue(actiual);
         }
     }
 }
