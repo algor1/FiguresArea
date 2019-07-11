298b4804d8e9b09ad2f39aeecdb0de065ab29473
algor1 <alexeev.igor.v@gmail.com>
2019-07-11 10:26:39 +0300

    no message

---
+++ FiguresAreaLib/FiguresAreaLib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureasArea
+{
+    public class FiguresAreaLib
+    {
+        public double CircleArea(double r)
+        {
+            if (r < 0) new ArgumentException("radius can't be negative");
+                       
+            return 2 * Math.PI * r * r;
+        }
+        public double TriangleArea(double a,double b,double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) new ArgumentException("Wrong size of triangle sides");
+
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+        }
+    }
+}
